3da68407a1a76674f5ee079cc693b311b8383a7e
Tiberiu Tasnadi <ttasnadi.olivera@gmail.com>
2020-11-29 01:09:45 +0100

    falta pulir front xd

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Web.Models;
+using VoidDetector;
+using System.IO;
 
 namespace Web.Controllers
 {
@@ -13,19 +15,25 @@ namespace Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Process()
         {
-            return View();
+            var file = Request.Form.Files;
+            List<Results> results = VoidDetector.Program.Start(file[0].FileName);
+
+            ResumViewModel resumViewModel = new ResumViewModel(results);
+
+            resumViewModel.FileName = file[0].FileName;
+            resumViewModel.Obstruction = results.Where(x => x.prediction == "obstruction").Count();
+            resumViewModel.ObstructionStr = resumViewModel.Obstruction > 0 ? "Si" : "No";
+            resumViewModel.Empty = results.Where(x => x.prediction == "empty").Count();
+            resumViewModel.Full = results.Where(x => x.prediction == "full").Count();
+
+            return View(resumViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ Web/Models/ResumViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using VoidDetector;
+
+namespace Web.Models
+{
+    public class ResumViewModel
+    {
+        [DisplayName("File: ")]
+        public string FileName { get; set; }
+        public string ImgPath { get; set; }
+        [DisplayName("Obstructions: ")]
+        public string ObstructionStr { get; set; }
+        public int Obstruction { get; set; }
+        [DisplayName("Empty spaces: ")]
+        public int Empty { get; set; }
+        public int Full { get; set; }
+
+        List<Results> Results { get; set; }
+
+        public ResumViewModel(List<Results> results)
+        {
+            this.Results = results;
+        }
+
+    }
+}
